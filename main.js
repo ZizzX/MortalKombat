4708fc2434353bdc0b0703f23efda5aaaa857751
Aziz Isapov <zizzxiii@gmail.com>
2021-06-04 00:57:10 +0600

    correction

--- main.js
+++ main.js
@@ -1,47 +1,59 @@
 const Scorpion = {
     name: 'Scorpion',
-    hp: 100,
+    hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-    weapon: 'cunai',
+    weapon: ['cunai'],
     attack: function() {
         console.log(`${this.name} Fight...`)
     }
 }
 
-function createPlayer(playerClass, playerName, playerHealth) {
+const kitana = {
+    name: 'kitana',
+    hp: 40,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    weapon: ['legs', 'guns'],
+    attack: function() {
+        console.log(`${this.name} Fight...`)
+    }
+}
+
+function createPlayer(playerClass, playerName, playerHealth, playerImg) {
     const player = document.createElement('div');
-    player1.classList.add(`.${playerClass}`);
+    player.classList.add(`${playerClass}`);
 
     const progressbar = document.createElement('div');
-    progressbar.classList.add('.progressbar');
-
-    const character = document.createElement('div');
-    character.classList.add('.character');
-
-    player.appendChild(progressbar);
-    player.appendChild(character);
-    
+    progressbar.classList.add('progressbar');
 
     const life = document.createElement('div');
-    life.classList.add('.life');
-    life.style.width = `${playerHealth}`;
+    life.classList.add('life');
+    life.style.width = `${playerHealth}%`;
 
     const name =  document.createElement('div');
-    name.classList.add('.name');
+    name.classList.add('name');
     name.textContent = `${playerName}`;
 
     progressbar.appendChild(life);
     progressbar.appendChild(name);
 
+
+    const character = document.createElement('div');
+    character.classList.add('character');
+
+    player.appendChild(character);
+    player.appendChild(progressbar);
+    
+    
+
     const img = document.createElement('img');
     character.appendChild(img);
-    img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+    img.src = `${playerImg}`;
     character.appendChild(img)
 
 
-    const arenas = document.createElement('div');
-    arenas.classList.add('.arenas');
+    const arenas = document.querySelector('.arenas');
     arenas.appendChild(player);
 }
 
-createPlayer('player1', Scorpion.name, Scorpion.hp);
+createPlayer('player1', Scorpion.name, Scorpion.hp, Scorpion.img);
+createPlayer('player2', kitana.name, kitana.hp, kitana.img);
