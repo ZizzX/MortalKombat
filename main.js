d29e146cb77b4c86fefcb8258b4f4eb534dd7fff
Aziz Isapov <zizzxiii@gmail.com>
2021-06-04 19:05:36 +0600

    modify

--- main.js
+++ main.js
@@ -1,59 +1,56 @@
-const Scorpion = {
-    name: 'Scorpion',
-    hp: 80,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-    weapon: ['cunai'],
-    attack: function() {
-        console.log(`${this.name} Fight...`)
-    }
-}
+const scorpion = {
+  name: "Scorpion",
+  hp: 80,
+  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+  weapon: ["cunai"],
+  attack: function () {
+    console.log(`${this.name} Fight...`);
+  },
+};
 
 const kitana = {
-    name: 'kitana',
-    hp: 40,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-    weapon: ['legs', 'guns'],
-    attack: function() {
-        console.log(`${this.name} Fight...`)
-    }
+  name: "Kitana",
+  hp: 40,
+  img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
+  weapon: ["legs", "guns"],
+  attack: function () {
+    console.log(`${this.name} Fight...`);
+  },
+};
+
+function createPlayer(
+  playerClass = "player1",
+  { name: playerName, hp: playerHealth = 100, img: playerImg } = {}
+) {
+  const player = document.createElement("div"),
+    arenas = document.querySelector(".arenas"),
+    progressbar = document.createElement("div"),
+    life = document.createElement("div"),
+    name = document.createElement("div"),
+    character = document.createElement("div"),
+    img = document.createElement("img");
+
+  player.classList.add(playerClass);
+  progressbar.classList.add("progressbar");
+  life.classList.add("life");
+  name.classList.add("name");
+  character.classList.add("character");
+
+  player.appendChild(character);
+  player.appendChild(progressbar);
+
+  progressbar.appendChild(life);
+  progressbar.appendChild(name);
+
+  character.appendChild(img);
+  character.appendChild(img);
+
+  life.style.width = `${playerHealth}%`;
+  name.textContent = `${playerName}`;
+  img.src = `${playerImg}`;
+
+  arenas.appendChild(player);
 }
 
-function createPlayer(playerClass, playerName, playerHealth, playerImg) {
-    const player = document.createElement('div');
-    player.classList.add(`${playerClass}`);
-
-    const progressbar = document.createElement('div');
-    progressbar.classList.add('progressbar');
-
-    const life = document.createElement('div');
-    life.classList.add('life');
-    life.style.width = `${playerHealth}%`;
-
-    const name =  document.createElement('div');
-    name.classList.add('name');
-    name.textContent = `${playerName}`;
-
-    progressbar.appendChild(life);
-    progressbar.appendChild(name);
-
-
-    const character = document.createElement('div');
-    character.classList.add('character');
-
-    player.appendChild(character);
-    player.appendChild(progressbar);
-    
-    
-
-    const img = document.createElement('img');
-    character.appendChild(img);
-    img.src = `${playerImg}`;
-    character.appendChild(img)
-
-
-    const arenas = document.querySelector('.arenas');
-    arenas.appendChild(player);
-}
-
-createPlayer('player1', Scorpion.name, Scorpion.hp, Scorpion.img);
-createPlayer('player2', kitana.name, kitana.hp, kitana.img);
+createPlayer("player1", scorpion);
+createPlayer("player2", kitana);
